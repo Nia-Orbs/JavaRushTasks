f2f2c59c821388586cdf80dd73362c048520a734
Nia-Orbs <>
2020-08-12 14:22:54 +0300

    Задание 5(вроде как...)

---
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -0,0 +1,21 @@
+package com.javarush.games.minesweeper;
+
+public class GameObject {
+
+    public int x;
+
+    public int y;
+
+    public boolean isMine;
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public GameObject(int x, int y, boolean isMine) {
+        this.x = x;
+        this.y = y;
+        this.isMine = isMine;
+    }
+}
