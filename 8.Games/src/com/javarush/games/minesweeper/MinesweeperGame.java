ac14a8f7c92dea05e202b17119466105f9085639
Nia-Orbs <>
2020-08-12 14:58:16 +0300

    Залила 6е и оно там все апдейтнуло само...

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -1,13 +1,14 @@
 package com.javarush.games.minesweeper;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinesweeperGame extends Game {
-
-    private final static int SIDE = 9;
-
+    private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
-
     private int countMinesOnField;
 
     @Override
@@ -16,19 +17,35 @@ public class MinesweeperGame extends Game {
         createGame();
     }
 
-    private void createGame(){
-        int count = 0;
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                boolean isMine = i == getRandomNumber(10);
-                gameField[i][j] = new GameObject(j, i, isMine);
+    private void createGame() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                boolean isMine = getRandomNumber(10) < 1;
                 if (isMine) {
-                    count++;
+                    countMinesOnField++;
                 }
-                setCellColor(i, j, Color.CORAL);
+                gameField[y][x] = new GameObject(x, y, isMine);
+                setCellColor(x, y, Color.ORANGE);
             }
         }
+    }
 
-        countMinesOnField = count;
+    private List<GameObject> getNeighbors(GameObject gameObject) {
+        List<GameObject> result = new ArrayList<>();
+        for (int y = gameObject.y - 1; y <= gameObject.y + 1; y++) {
+            for (int x = gameObject.x - 1; x <= gameObject.x + 1; x++) {
+                if (y < 0 || y >= SIDE) {
+                    continue;
+                }
+                if (x < 0 || x >= SIDE) {
+                    continue;
+                }
+                if (gameField[y][x] == gameObject) {
+                    continue;
+                }
+                result.add(gameField[y][x]);
+            }
+        }
+        return result;
     }
 }
